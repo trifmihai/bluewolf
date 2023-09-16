28c7579ff3ed4c6b086721c1a189c19070d2c0a9
mike_trif <97960573+trifmihai@users.noreply.github.com>
2023-09-16 23:13:05 +0300

    Specific project page slider inclusion

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@
   new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
 
   // src/index.ts
-  $(".projects-gallery_component").each(function(index) {
+  $(".projects-gallery_component.is-projects").each(function(index) {
     const totalSlides = $(this).find(".swiper-slide.is-slider-projects").length;
     $(".swiper-number-total").text(totalSlides);
     const swiper = new Swiper($(this).find(".swiper.is-slider-projects")[0], {
@@ -15,6 +15,33 @@
         // when window width is >=
         991: {
           slidesPerView: 2,
+          allowTouchMove: false,
+          slideToClickedSlide: true
+        }
+      },
+      speed: 400,
+      loop: true,
+      navigation: {
+        nextEl: $(this).find(".swiper-next")[0],
+        prevEl: $(this).find(".swiper-prev")[0]
+      }
+    });
+    swiper.on("slideChange", function(e) {
+      const slideNumber = e.realIndex + 1;
+      $(".swiper-number-current").text(slideNumber);
+    });
+  });
+  $(".projects-gallery_component.is-gallery").each(function(index) {
+    const totalSlides = $(this).find(".swiper-slide.is-slider-gallery").length;
+    $(".swiper-number-total").text(totalSlides);
+    const swiper = new Swiper($(this).find(".swiper.is-slider-gallery")[0], {
+      slidesPerView: 1,
+      spaceBetween: 24,
+      allowTouchMove: true,
+      breakpoints: {
+        // when window width is >=
+        991: {
+          slidesPerView: 3,
           allowTouchMove: false,
           slideToClickedSlide: true
         }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-$('.projects-gallery_component').each(function (index) {
+$('.projects-gallery_component.is-projects').each(function (index) {
   const totalSlides = $(this).find('.swiper-slide.is-slider-projects').length;
   $('.swiper-number-total').text(totalSlides);
   const swiper = new Swiper($(this).find('.swiper.is-slider-projects')[0], {
@@ -9,6 +9,35 @@ $('.projects-gallery_component').each(function (index) {
       // when window width is >=
       991: {
         slidesPerView: 2,
+        allowTouchMove: false,
+        slideToClickedSlide: true,
+      },
+    },
+
+    speed: 400,
+    loop: true,
+    navigation: {
+      nextEl: $(this).find('.swiper-next')[0],
+      prevEl: $(this).find('.swiper-prev')[0],
+    },
+  });
+  swiper.on('slideChange', function (e) {
+    const slideNumber = e.realIndex + 1;
+    $('.swiper-number-current').text(slideNumber);
+  });
+});
+
+$('.projects-gallery_component.is-gallery').each(function (index) {
+  const totalSlides = $(this).find('.swiper-slide.is-slider-gallery').length;
+  $('.swiper-number-total').text(totalSlides);
+  const swiper = new Swiper($(this).find('.swiper.is-slider-gallery')[0], {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    allowTouchMove: true,
+    breakpoints: {
+      // when window width is >=
+      991: {
+        slidesPerView: 3,
         allowTouchMove: false,
         slideToClickedSlide: true,
       },
