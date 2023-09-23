c4468358e8fc04d97064b844e7580ee769f60821
mike_trif <97960573+trifmihai@users.noreply.github.com>
2023-09-23 14:23:41 +0300

    Added different sizes for projects and gallery

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,22 @@
 
   // src/index.ts
   document.addEventListener("DOMContentLoaded", function() {
-    const cmsImages = document.querySelectorAll(".image_absolute");
+    const cmsImages = document.querySelectorAll(".image_projects");
     cmsImages.forEach(function(img) {
       img.setAttribute(
         "sizes",
-        "(max-width: 479px) 44vw, (max-width: 767px) 47vw, (max-width: 991px) 46vw, (max-width: 1919px) 40vw, 520px"
+        //'(max-width: 479px) 44vw, (max-width: 767px) 47vw, (max-width: 991px) 46vw, (max-width: 1919px) 40vw, 520px'
+        "(max-width: 479px) 95vw, (max-width: 767px) 90vw, (max-width: 1023px) 48vw, (max-width: 1440px) 48vw, (max-width: 1919px) 48vw, 50vw"
+      );
+    });
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    const cmsImages = document.querySelectorAll(".image_gallery");
+    cmsImages.forEach(function(img) {
+      img.setAttribute(
+        "sizes",
+        //'(max-width: 479px) 44vw, (max-width: 767px) 47vw, (max-width: 991px) 46vw, (max-width: 1919px) 40vw, 520px'
+        "(max-width: 479px) 95vw, (max-width: 567px) 92vw, (max-width: 767px) 90vw, (max-width: 1023px) 33vw, (max-width: 1440px) 33vw, (max-width: 1919px) 33vw,  33vw"
       );
     });
   });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Select all CMS images with a specific class
-  const cmsImages = document.querySelectorAll('.image_absolute');
+  const cmsImages = document.querySelectorAll('.image_projects');
 
   cmsImages.forEach(function (img) {
     // Add/Override the sizes attribute
     img.setAttribute(
       'sizes',
-      '(max-width: 479px) 44vw, (max-width: 767px) 47vw, (max-width: 991px) 46vw, (max-width: 1919px) 40vw, 520px'
+      //'(max-width: 479px) 44vw, (max-width: 767px) 47vw, (max-width: 991px) 46vw, (max-width: 1919px) 40vw, 520px'
+      '(max-width: 479px) 95vw, (max-width: 767px) 90vw, (max-width: 1023px) 48vw, (max-width: 1440px) 48vw, (max-width: 1919px) 48vw, 50vw'
+    );
+
+    // Optionally, you can also set/override the srcset attribute here
+    // img.setAttribute('srcset', '...');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Select all CMS images with a specific class
+  const cmsImages = document.querySelectorAll('.image_gallery');
+
+  cmsImages.forEach(function (img) {
+    // Add/Override the sizes attribute
+    img.setAttribute(
+      'sizes',
+      //'(max-width: 479px) 44vw, (max-width: 767px) 47vw, (max-width: 991px) 46vw, (max-width: 1919px) 40vw, 520px'
+      '(max-width: 479px) 95vw, (max-width: 567px) 92vw, (max-width: 767px) 90vw, (max-width: 1023px) 33vw, (max-width: 1440px) 33vw, (max-width: 1919px) 33vw,  33vw'
     );
 
     // Optionally, you can also set/override the srcset attribute here
