b1525c5f84e00e465adfc0ea91bbfa0edf277c20
mike_trif <97960573+trifmihai@users.noreply.github.com>
2023-09-23 15:04:52 +0300

    Separating swiper from the rest of the code

--- dist/index.js
+++ dist/index.js
@@ -4,26 +4,6 @@
   new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
 
   // src/index.ts
-  document.addEventListener("DOMContentLoaded", function() {
-    const cmsImages = document.querySelectorAll(".image_projects");
-    cmsImages.forEach(function(img) {
-      img.setAttribute(
-        "sizes",
-        //'(max-width: 479px) 44vw, (max-width: 767px) 47vw, (max-width: 991px) 46vw, (max-width: 1919px) 40vw, 520px'
-        "(max-width: 479px) 95vw, (max-width: 767px) 90vw, (max-width: 1023px) 48vw, (max-width: 1440px) 48vw, (max-width: 1919px) 48vw, 50vw"
-      );
-    });
-  });
-  document.addEventListener("DOMContentLoaded", function() {
-    const cmsImages = document.querySelectorAll(".image_gallery");
-    cmsImages.forEach(function(img) {
-      img.setAttribute(
-        "sizes",
-        //'(max-width: 479px) 44vw, (max-width: 767px) 47vw, (max-width: 991px) 46vw, (max-width: 1919px) 40vw, 520px'
-        "(max-width: 479px) 95vw, (max-width: 567px) 92vw, (max-width: 767px) 90vw, (max-width: 1023px) 33vw, (max-width: 1440px) 33vw, (max-width: 1919px) 33vw,  33vw"
-      );
-    });
-  });
   $(".projects-gallery_component.is-projects").each(function(index) {
     const totalSlides = $(this).find(".swiper-slide.is-slider-projects").length;
     $(".swiper-number-total").text(totalSlides);
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,58 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Select all CMS images with a specific class
-  const cmsImages = document.querySelectorAll('.image_projects');
+$('.projects-gallery_component.is-projects').each(function (index) {
+  const totalSlides = $(this).find('.swiper-slide.is-slider-projects').length;
+  $('.swiper-number-total').text(totalSlides);
+  const swiper = new Swiper($(this).find('.swiper.is-slider-projects')[0], {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    allowTouchMove: true,
+    breakpoints: {
+      // when window width is >=
+      991: {
+        slidesPerView: 2,
+        allowTouchMove: false,
+        slideToClickedSlide: false,
+      },
+    },
 
-  cmsImages.forEach(function (img) {
-    // Add/Override the sizes attribute
-    img.setAttribute(
-      'sizes',
-      //'(max-width: 479px) 44vw, (max-width: 767px) 47vw, (max-width: 991px) 46vw, (max-width: 1919px) 40vw, 520px'
-      '(max-width: 479px) 95vw, (max-width: 767px) 90vw, (max-width: 1023px) 48vw, (max-width: 1440px) 48vw, (max-width: 1919px) 48vw, 50vw'
-    );
-
-    // Optionally, you can also set/override the srcset attribute here
-    // img.setAttribute('srcset', '...');
+    speed: 400,
+    loop: true,
+    navigation: {
+      nextEl: $(this).find('.swiper-next')[0],
+      prevEl: $(this).find('.swiper-prev')[0],
+    },
+  });
+  swiper.on('slideChange', function (e) {
+    const slideNumber = e.realIndex + 1;
+    $('.swiper-number-current').text(slideNumber);
   });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  // Select all CMS images with a specific class
-  const cmsImages = document.querySelectorAll('.image_gallery');
+$('.projects-gallery_component.is-gallery').each(function (index) {
+  const totalSlides = $(this).find('.swiper-slide.is-slider-gallery').length;
+  $('.swiper-number-total').text(totalSlides);
+  const swiper = new Swiper($(this).find('.swiper.is-slider-gallery')[0], {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    allowTouchMove: true,
+    breakpoints: {
+      // when window width is >=
+      991: {
+        slidesPerView: 3,
+        grabCursor: true,
+        allowTouchMove: true,
+        slideToClickedSlide: false,
+      },
+    },
 
-  cmsImages.forEach(function (img) {
-    // Add/Override the sizes attribute
-    img.setAttribute(
-      'sizes',
-      //'(max-width: 479px) 44vw, (max-width: 767px) 47vw, (max-width: 991px) 46vw, (max-width: 1919px) 40vw, 520px'
-      '(max-width: 479px) 95vw, (max-width: 567px) 92vw, (max-width: 767px) 90vw, (max-width: 1023px) 33vw, (max-width: 1440px) 33vw, (max-width: 1919px) 33vw,  33vw'
-    );
-
-    // Optionally, you can also set/override the srcset attribute here
-    // img.setAttribute('srcset', '...');
+    speed: 400,
+    loop: true,
+    navigation: {
+      nextEl: $(this).find('.swiper-next')[0],
+      prevEl: $(this).find('.swiper-prev')[0],
+    },
+  });
+  swiper.on('slideChange', function (e) {
+    const slideNumber = e.realIndex + 1;
+    $('.swiper-number-current').text(slideNumber);
   });
 });
