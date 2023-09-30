fe307d4e746f84e47dc39a0837940d314fc9d779
mike_trif <97960573+trifmihai@users.noreply.github.com>
2023-09-30 04:33:50 +0300

    Fixed sizes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-/* 
-
 // defer
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -34,73 +32,73 @@ document.addEventListener('DOMContentLoaded', function () {
     // Optionally, you can also set/override the srcset attribute here
     // img.setAttribute('srcset', '...');
   });
-}); */
-
-document.addEventListener('DOMContentLoaded', () => {
-  // Get the parent container
-  const container = document.querySelector('.slider-gallery_numbers');
-
-  if (container && container.innerHTML) {
-    // Replace the dash character with "of"
-    container.innerHTML = container.innerHTML.replace(/—/g, 'of');
-  }
 });
 
-$('.projects-gallery_component.is-projects').each(function (index) {
-  const totalSlides = $(this).find('.swiper-slide.is-slider-projects').length;
-  $('.swiper-number-total').text(totalSlides);
-  const swiper = new Swiper($(this).find('.swiper.is-slider-projects')[0], {
-    slidesPerView: 1,
-    spaceBetween: 24,
-    allowTouchMove: true,
-    breakpoints: {
-      // when window width is >=
-      991: {
-        slidesPerView: 2,
-        allowTouchMove: false,
-        slideToClickedSlide: false,
-      },
-    },
+// document.addEventListener('DOMContentLoaded', () => {
+//   // Get the parent container
+//   const container = document.querySelector('.slider-gallery_numbers');
 
-    speed: 400,
-    loop: true,
-    navigation: {
-      nextEl: $(this).find('.swiper-next')[0],
-      prevEl: $(this).find('.swiper-prev')[0],
-    },
-  });
-  swiper.on('slideChange', function (e) {
-    const slideNumber = e.realIndex + 1;
-    $('.swiper-number-current').text(slideNumber);
-  });
-});
+//   if (container && container.innerHTML) {
+//     // Replace the dash character with "of"
+//     container.innerHTML = container.innerHTML.replace(/—/g, 'of');
+//   }
+// });
 
-$('.projects-gallery_component.is-gallery').each(function (index) {
-  const totalSlides = $(this).find('.swiper-slide.is-slider-gallery').length;
-  $('.swiper-number-total').text(totalSlides);
-  const swiper = new Swiper($(this).find('.swiper.is-slider-gallery')[0], {
-    slidesPerView: 1,
-    spaceBetween: 24,
-    allowTouchMove: true,
-    breakpoints: {
-      // when window width is >=
-      991: {
-        slidesPerView: 3,
-        grabCursor: true,
-        allowTouchMove: true,
-        slideToClickedSlide: false,
-      },
-    },
+// $('.projects-gallery_component.is-projects').each(function (index) {
+//   const totalSlides = $(this).find('.swiper-slide.is-slider-projects').length;
+//   $('.swiper-number-total').text(totalSlides);
+//   const swiper = new Swiper($(this).find('.swiper.is-slider-projects')[0], {
+//     slidesPerView: 1,
+//     spaceBetween: 24,
+//     allowTouchMove: true,
+//     breakpoints: {
+//       // when window width is >=
+//       991: {
+//         slidesPerView: 2,
+//         allowTouchMove: false,
+//         slideToClickedSlide: false,
+//       },
+//     },
 
-    speed: 400,
-    loop: true,
-    navigation: {
-      nextEl: $(this).find('.swiper-next')[0],
-      prevEl: $(this).find('.swiper-prev')[0],
-    },
-  });
-  swiper.on('slideChange', function (e) {
-    const slideNumber = e.realIndex + 1;
-    $('.swiper-number-current').text(slideNumber);
-  });
-});
+//     speed: 400,
+//     loop: true,
+//     navigation: {
+//       nextEl: $(this).find('.swiper-next')[0],
+//       prevEl: $(this).find('.swiper-prev')[0],
+//     },
+//   });
+//   swiper.on('slideChange', function (e) {
+//     const slideNumber = e.realIndex + 1;
+//     $('.swiper-number-current').text(slideNumber);
+//   });
+// });
+
+// $('.projects-gallery_component.is-gallery').each(function (index) {
+//   const totalSlides = $(this).find('.swiper-slide.is-slider-gallery').length;
+//   $('.swiper-number-total').text(totalSlides);
+//   const swiper = new Swiper($(this).find('.swiper.is-slider-gallery')[0], {
+//     slidesPerView: 1,
+//     spaceBetween: 24,
+//     allowTouchMove: true,
+//     breakpoints: {
+//       // when window width is >=
+//       991: {
+//         slidesPerView: 3,
+//         grabCursor: true,
+//         allowTouchMove: true,
+//         slideToClickedSlide: false,
+//       },
+//     },
+
+//     speed: 400,
+//     loop: true,
+//     navigation: {
+//       nextEl: $(this).find('.swiper-next')[0],
+//       prevEl: $(this).find('.swiper-prev')[0],
+//     },
+//   });
+//   swiper.on('slideChange', function (e) {
+//     const slideNumber = e.realIndex + 1;
+//     $('.swiper-number-current').text(slideNumber);
+//   });
+// });
